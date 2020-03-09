c600443b91cb4390d2e8b5f68134f406b7719af2
shtolfensie <31325217+shtolfensie@users.noreply.github.com>
2020-03-09 23:31:52 +0100

    Basic firebase setup; Using React context to provide firebase; Basic user login/logout setup with working header avatar and button based on auth

---
+++ app/src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import auth from 'firebase/auth';
+import database from 'firebase/database'
+
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+}
+
+class Firebase {
+  constructor() {
+    app.initializeApp(config);
+
+    this.auth = app.auth();
+    this.db = app.database();
+    this.providerGoogle = new app.auth.GoogleAuthProvider();
+
+  }
+
+  doSignInWithGoogle = () => {
+    this.auth.signInWithPopup(this.providerGoogle).then(result => {
+      let token = result.credential.accessToken;
+      let user = result.user;
+    })
+    .catch(error => console.error(error.code, error.message))
+  }
+  doSignOut = () => {
+    this.auth.signOut();
+  }
+}
+
+export default Firebase;
--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FirebaseContext } from './Firebase';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar } from '@material-ui/core';
-import Typography from '@material-ui/core/Typography';
+import { AppBar, Toolbar, Avatar } from '@material-ui/core';
 import { Button } from '@material-ui/core';
 import { Menu, MenuItem, IconButton } from '@material-ui/core'
 import AccountCircle from '@material-ui/icons/AccountCircle';
@@ -12,11 +12,16 @@ const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1,
   },
+  small: {
+    width: theme.spacing(3),
+    height: theme.spacing(3),
+    cursor: 'pointer'
+  }
 }));
 
 const Header = ({handleInterpreterChange, handleEditorChange, isEditorClassic, isInterpreterJSlike}) => {
   const classes = useStyles();
-  const [auth, setAuth] = useState(true);
+  const [user, setUser] = useState(false);
   
 
   return (
@@ -26,28 +31,35 @@ const Header = ({handleInterpreterChange, handleEditorChange, isEditorClassic, i
           <AorBSwitch a='Schemy' b='JS-Like' handleChange={handleInterpreterChange} isChecked={isInterpreterJSlike}/>
           <AorBSwitch a='jupy' b='classic' handleChange={handleEditorChange} isChecked={isEditorClassic}/>
           <div className={classes.grow}/>
-          {auth ? <AccountAvatarAndMenu /> : <Button color='inherit'>Login</Button>}
+          <FirebaseContext.Consumer>
+            {firebase => <Login firebase={firebase}/>}
+
+          </FirebaseContext.Consumer>
         </Toolbar>
       </AppBar>
     </div>
   );
 }
 
-const AccountAvatarAndMenu = () => {
+const AccountAvatarAndMenu = ({handleSignOut, userPhotoURL}) => {
+  const classes = useStyles();
   const [anchorElement, setAnchorElement] = useState(null);
   const isMenuOpen = Boolean(anchorElement);
 
   const handleMenuOpen = e => setAnchorElement(e.currentTarget);
   const handleMenuClose = () => setAnchorElement(null);
-
+  // userPhotoURL = false;
   return (
     <div>
-      <IconButton
-        color='inherit'
-        onClick={handleMenuOpen}
-      >
-        <AccountCircle/>
-      </IconButton>
+      { userPhotoURL
+      ? <Avatar onClick={handleMenuOpen} className={classes.small} src={userPhotoURL}/>
+      : <IconButton
+          color='inherit'
+          onClick={handleMenuOpen}
+        >
+          <AccountCircle/>
+        </IconButton>
+      }
       <Menu
         id="menu-appbar"
         anchorEl={anchorElement}
@@ -63,10 +75,30 @@ const AccountAvatarAndMenu = () => {
         open={isMenuOpen}
         onClose={handleMenuClose}
       >
-        <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-        <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+        <MenuItem onClick={handleMenuClose} onClick={handleSignOut}>Sign Out</MenuItem>
+        {/* <MenuItem onClick={handleMenuClose}>My account</MenuItem> */}
       </Menu>
     </div>
+  )
+}
+
+const Login = ({firebase}) => {
+  const [authUser, setAuthUser] = useState(null);
+
+  useEffect(() => {
+    console.log('user effect')
+    console.log(authUser)
+    firebase.auth.onAuthStateChanged(user => user ? setAuthUser(user) : setAuthUser(null));
+  })
+
+  const handleSignOut = () => {
+    firebase.doSignOut();
+  }
+
+  return (
+    authUser
+      ? <AccountAvatarAndMenu handleSignOut={handleSignOut} userPhotoURL={authUser.photoURL}/>
+      : <Button color='inherit' onClick={firebase.doSignInWithGoogle}>Login</Button>
   )
 }
 
