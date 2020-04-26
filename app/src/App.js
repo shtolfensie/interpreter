82797f8f85c412581299495639d0ead2fe0f086a
shtolfensie <31325217+shtolfensie@users.noreply.github.com>
2020-04-26 23:33:29 +0200

    Removed editor classic

--- app/src/App.js
+++ app/src/App.js
@@ -16,22 +16,17 @@ const theme = createMuiTheme({
 
 const App = () => {
   const [isInterpreterJSlike, setIsIetinterpreterJSlike] = useState(false)
-  const [isEditorClassic, setIsEditorClassic] = useState(false)
 
   const handleInterpreterChange = e => setIsIetinterpreterJSlike(e.target.checked);
-  const handleEditorChange = e => setIsEditorClassic(e.target.checked);
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <Header
           handleInterpreterChange={handleInterpreterChange}
-          handleEditorChange={handleEditorChange}
           isInterpreterJSlike={isInterpreterJSlike}
-          isEditorClassic={isEditorClassic}
         />
         <EditorContainer
           interpreter={isInterpreterJSlike ? 'jsl' : 'sch'}
-          editor={isEditorClassic ? 'classic' : 'jupy'}
         />
       </ThemeProvider>
     </div>
